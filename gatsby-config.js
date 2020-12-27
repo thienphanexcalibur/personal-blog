9842dfcc48d3be3fbeb78fd57e828435f5546f7d
thienphanexcalibur <thienkphan@gmail.com>
2020-12-28 00:31:03 +0700

    feat: add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-117668794-1`,
       },
     },
     `gatsby-plugin-feed`,
